f715a3584d72e71aaf0be0258a5725059a3b389f
Jacob Vega <jacob.vega@gmail.com>
2024-02-19 20:00:47 -0300

    Versión inicial de la pantalla Fullscreen

---
+++ src/app/maps/maps.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+import { MapsRoutingModule } from './maps-routing.module';
+
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiamFjb2J2ZWdhIiwiYSI6ImNsc3NieHpyYTBkazQyamxvZjY5MGdxeGQifQ.MkOFifLlJLTEg5aWg2zqAQ';
+
+
+@NgModule({
+  declarations: [
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent,
+    FullScreenPageComponent,
+    MarkersPageComponent,
+    PropertiesPageComponent,
+    ZoomRangePageComponent
+  ],
+  imports: [
+    CommonModule, 
+    MapsRoutingModule
+  ]
+})
+export class MapsModule { }
